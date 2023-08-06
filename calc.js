2742321ba0189243f190b7b7d44e3d419ee7f5d4
Zsolt Klauzer <99751489+zsklz@users.noreply.github.com>
2023-08-06 15:59:05 +0200

    Add operation object and operate() function

--- calc.js
+++ calc.js
@@ -1,1 +1,25 @@
-console.log("Hello world");
+const operation = {
+    n0: undefined,
+    n1: undefined,
+    operator: undefined
+}
+
+function operate(operation) {
+    switch (operation.operator) {
+    case '+':
+	return operation.n0 + operation.n1;
+	break;
+    case '-':
+	return operation.n0 - operation.n1;
+	break;
+    case '*':
+	return operation.n0 * operation.n1;
+	break;
+    case '/':
+	return operation.n0 / operation.n1;
+	break;
+    default:
+	return undefined;
+    }
+}
+
