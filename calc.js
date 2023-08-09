76a5c5df5637dced232094fb07c24255a3285356
Zsolt Klauzer <99751489+zsklz@users.noreply.github.com>
2023-08-09 16:26:41 +0200

    Add display logic for main and secondary displays

--- calc.js
+++ calc.js
@@ -2,23 +2,50 @@ const display = app.querySelector('#display');
 const mainDisplay = display.querySelector('#main');
 const secondaryDisplay = display.querySelector('#secondary');
 const digits = app.querySelectorAll('.btn.digit');
+const operators = app.querySelectorAll('.btn.operator');
 const decimalPoint = app.querySelector('#decimal');
+const equal = app.querySelector('#equal');
 let isDecimal = false;
+let operationPending = false;
 
 digits.forEach((button) => {
     button.addEventListener('click', () => {
 	// TODO make event listener a separate function
+	if (operationPending){
+	    mainDisplay.textContent = "";
+	    operationPending = false;
+	}
 	if (mainDisplay.textContent.length < 16) {
 	    mainDisplay.textContent += `${event.target.textContent}`;
 	}
     });
 });
 
-decimalPoint.addEventListener('click', () => {
+decimalPoint.addEventListener('click', (event) => {
     if (mainDisplay.textContent.length < 16 && !isDecimal) {
+	if (operationPending) {
+	    mainDisplay.textContent = "";
+	    operationPending = false;
+	}
 	isDecimal = true;
 	mainDisplay.textContent += `${event.target.textContent}`;
     }
+});
+
+operators.forEach((operator) => {
+    operator.addEventListener('click', (event) => {
+	if (mainDisplay.textContent) {
+	    secondaryDisplay.textContent = `${mainDisplay.textContent} ${event.target.textContent}`;
+	    operation.n0 = Number(mainDisplay.textContent);
+	    operation.operator = event.target.textContent;
+	    isDecimal = false;
+	    operationPending = true;
+	}
+    });
+});
+
+equal.addEventListener('click', () => {
+    
 });
 
 const operation = {
